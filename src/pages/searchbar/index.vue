9190f0c95595a96d5fe83d57ee8af6e64cb5b150
xuebw <Poer666>
2019-06-12 17:37:28 +0800

    Initial commit

---
+++ src/pages/searchbar/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="counter-warp">
+    1236
+  </div>
+</template>
+
+<script>
+
+export default {
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+.counter-warp {
+  text-align: center;
+  margin-top: 100px;
+}
+.home {
+  display: inline-block;
+  margin: 100px auto;
+  padding: 5px 10px;
+  color: blue;
+  border: 1px solid blue;
+}
+</style>
